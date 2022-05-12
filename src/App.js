1abc81bd5a6d1f83c102cea7eb9b692f8568c2f3
sava <savamedvedev@gmail.com>
2022-05-12 19:15:10 +0800

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import Navbar from "./components/navbar/Navbar";
-
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Recommend from "./components/Recommend";
+import ScrollToTop from "./components/ScrollToTop";
+import Services from "./components/Services";
+import Testimonials from "./components/Testimonials";
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
+      {/* <ScrollToTop />
+      <Services />
+      <Recommend />
+      <Testimonials />
+      <Footer /> */}
     </>
   );
 }
